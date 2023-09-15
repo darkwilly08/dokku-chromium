4b1be0fbbbf52157edef7f6949779901e91cc967
Franco Berardi <darkwilly08@gmail.com>
2023-09-14 22:46:14 -0300

    feat: pdf generator  added

--- src/services/pdfService.ts
+++ src/services/pdfService.ts
@@ -1,3 +1,22 @@
-export const generate = async () => {
-  return `<body> <h1> Hello World </h1> </body>`;
-};
+import { generatePdf } from 'html-pdf-node';
+
+export const generate = () =>
+  new Promise<string>((resolve, reject) => {
+    generatePdf(
+      {
+        content: `<body> <h1> Hello World </h1> </body>`,
+      },
+      {
+        width: 1280,
+        height: 720,
+        printBackground: true,
+      },
+      (err: Error, buffer: Buffer) => {
+        if (err) {
+          return reject(err);
+        }
+
+        return resolve(buffer.toString('base64'));
+      },
+    );
+  });
